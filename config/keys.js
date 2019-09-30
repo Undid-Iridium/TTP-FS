4d1f09cb61e5897e88a53f5fc851e7ccfdde8ba3
Xavier <tuxwonder7@gmail.com>
2019-09-30 12:15:42 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : "mongodb+srv://FrancisXAdmin:1willnotdie12@@ttpstockapp-dosfe.mongodb.net/test?retryWrites=true",
+    mongoURI : "",
     secretOrKey: "secret"
 };
